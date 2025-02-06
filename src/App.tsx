ab6cc1002a9e1bd30662f48282c75cc5b38f13cc
SaimRazaAly <saimrazateach2010@gmail.com>
2025-02-06 09:12:43 -0800

    website done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Download from "./sections/Download";
 import Faq from "./sections/Faq";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Pricing from "./sections/Pricing";
@@ -13,6 +14,7 @@ function App (){
     <Pricing/>
     <Faq/>
     <Download/>
+    <Footer/>
     </>
   )
 }
